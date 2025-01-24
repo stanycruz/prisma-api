1f36a97dd0c38c4c9ca1e0398ea602a0e6c74468
Stanislaw Cruz <stanycruz@outlook.com>
2025-01-24 20:45:17 -0300

    feat: update CRUD resource of posts

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,11 @@
-export class Post {}
+import { Post } from '@prisma/client';
+
+export class PostEntity implements Post {
+  id: number;
+  published: boolean;
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  authorId: number;
+}
