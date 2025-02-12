d8f03741e1db809b215cd180040b5c26b5a1f161
Stanislaw Cruz <stanycruz@outlook.com>
2025-02-12 13:24:00 -0300

    feat(posts): add controller and service

--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -2,7 +2,9 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { PostEntity } from '../entities/post.entity';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
